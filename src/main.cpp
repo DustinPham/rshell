37aad83ab22e9cd5a748db20eba4beb40004dceb
Dustin Pham <dpham008@ucr.edu>
2014-10-18 17:09:55 -0700

    Updated main.cpp to work with single commands

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 int main()
 {
     int pid = fork();
+
     if (pid == -1)
     {
         perror("There was an error with fork().");
@@ -19,27 +20,31 @@ int main()
     {
         cout << "$ ";
         char input[1024] = {0};
-        
+        cin.getline(input, 1024);
+
         char* parse;
         char** store;
         store = new char *[1024];
-        
+
         int i = 0;
-        
-        parse = strtok(input, " ");
-        
+
+        parse = strtok(input, " ;");
+
         while (parse != NULL)
         {
             store[i] = parse;
+            cout << "This is in store[" << i << "]: " << store[i]  <<endl;
             i++;
-            parse = strtok(NULL, " ");
+            parse = strtok(NULL, " ;");
         }
-        
+
         if (-1 == execvp(store[0], store))
         {
             perror("There was an error in execvp.");
         }
-        
+
+        cout << "THIS IS THE END OF THE CHILD" << endl;
+
         exit(1);
     }
     else if (pid > 0)
@@ -50,6 +55,6 @@ int main()
         }
         cout << "THIS IS THE PARENT" << endl;
     }
-    
+
     return 0;
 }
